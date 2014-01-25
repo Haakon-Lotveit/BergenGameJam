4d4366d13f27f3acb85edae8e3b541869170fab3
Haakon Løtveit <haakon_lotveit@live.no>
2014-01-25 14:42:17 +0100

    Oppdaterte dialog

--- src/no/gamejam/TalkEngine.java
+++ src/no/gamejam/TalkEngine.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -68,6 +70,33 @@ public class TalkEngine {
 		return new Pair<Integer, String>(resultat, response);
 	}
 	
+	public String getResponse(String type, String subType){
+		if(dialog.containsKey(type)){
+			if(dialog.get(type).containsKey(subType)){
+				ArrayList<String> lst = dialog.get(type).get(subType);
+				return lst.get(r.nextInt(lst.size()));
+			}
+		}
+		return "[DEBUG]: Dialog ikke funnet. Finn Haakon og vis ham feilen!"; 
+	}
+	
+	public List<String> getResponses(String type, String subType){
+		if(dialog.containsKey(type)){
+			if(dialog.get(type).containsKey(subType)){
+				return dialog.get(type).get(subType);
+			}
+		}
+		List<String> error = new LinkedList<>();
+		error.add("[DEBUG]: Dialog ikke funnet. Finn Haakon og vis ham feilen!");
+		return error;
+	}
+	
+	/**
+	 * Dette er en spesialisert metode for diplomati, med begrenset typesjekking.
+	 * @param type
+	 * @param result
+	 * @return
+	 */
 	private String getResponse(TalkType type, Integer result){
 		String resultat;
 		if(result < 0){
