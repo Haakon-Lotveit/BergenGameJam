e879b19970cdf7f3ff747fb1f72139c06b80fe8f
Haakon Løtveit <haakon_lotveit@live.no>
2014-01-25 17:19:29 +0100

    La til paintable grensesnittet

--- src/ghp/tilegame/main/levels/TiledLevel.java
+++ src/ghp/tilegame/main/levels/TiledLevel.java
@@ -3,7 +3,11 @@ package ghp.tilegame.main.levels;
 import ghp.tilegame.main.tiles.Tile;
 
 public interface TiledLevel {
-	public void setTile(int x, int y, Tile tile);
+	/*
+	 * Enten må konstruktøren la oss spesifisere størrelsen i antall tiles, eller så må den være et fast tall
+	 * Det letteste er fast tall
+	 */
+	public void setTile(int x, int y, Tile2D tile);
 	public Tile getTile(int x, int y);
 	public int getXSize();
 	public int getYSize();
