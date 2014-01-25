f525f00d91219d03f421c138158c4cada5127399
Haakon Løtveit <haakon_lotveit@live.no>
2014-01-25 13:29:31 +0100

    La til debug på imageloaderen

--- src/ghp/tilegame/main/gfx/ImageLoader.java
+++ src/ghp/tilegame/main/gfx/ImageLoader.java
@@ -9,8 +9,12 @@ import javax.imageio.ImageIO;
 public class ImageLoader
 {
 	public BufferedImage load(String path){
+		File imageFile = new File(path);
+		System.out.print("Finnes filen? ");
+		System.out.println(imageFile.exists());
+		System.out.println(imageFile.getAbsolutePath());
 		try {
-			return ImageIO.read(new File(path));
+			return ImageIO.read(imageFile);
 		} catch (IOException e) {e.printStackTrace();}
 		return null;
 	}
